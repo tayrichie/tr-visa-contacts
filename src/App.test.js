c72f1a7645edda9bb2ff7762b31c3f2895e14208
Taylor Richards <taylor.richards@deque.com>
2022-07-15 17:14:08 -0400

    fix: clean up package.json and unused test files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Add New Contact/i);
-  expect(linkElement).toBeInTheDocument();
-});
